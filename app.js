05462f9a4832cf8e8bfc98ce818799cf1df3fae5
muhammad haseen <hasinmmpm786@gmail.com>
2024-05-28 06:59:24 +0530

    define schema

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+const dotenv =require('dotenv')
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/nm', (req, res) => {
-    res.send('NM!')
-  })
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+main().then(
+    console.log("Connected"))
+    .catch(err => console.log(err));
+
+async function main() {
+    connectionstring = process.env.DB_string 
+  await mongoose.connect(connectionstring);
+}
---
+++ model/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+// const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type : String,
+        required : true,
+        minlength: 3,
+        uppercase: true
+        
+    },
+    lastName:{
+        type : String,
+        required : true,
+        minlength :  3,
+        uppercase : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type :  String,
+        required : true
+    }
+         });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User
+
+        
