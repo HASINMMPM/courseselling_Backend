bc3e8ad8847e584ffe63582228e7d0cac97a52b4
muhammad haseen <hasinmmpm786@gmail.com>
2024-07-05 16:54:16 +0530

    instructor router

---
+++ router/InstructorRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { signup,  instructorLogin } from "../controller/instructorController.js";
+
+
+const InstructorRouter = express();
+
+InstructorRouter.post('/signup',signup)
+InstructorRouter.post('/login',instructorLogin)
+
+
+
+export default InstructorRouter;
--- router/allRouter.js
+++ router/allRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import userRouter from './userRouter.js'
+import InstructorRouter from './InstructorRouter.js'
 
 const v1Router = express.Router()
 
 v1Router.use('/v1/user',userRouter)
+v1Router.use('/v1/instructer' , InstructorRouter)
 
 
 export default v1Router
